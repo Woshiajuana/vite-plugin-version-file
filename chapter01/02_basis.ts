56cd42e916b4ab84c57b034230d1f0207fdaae3a
Woshiajuana <zhigang.chen@owulia.com>
2021-04-22 14:24:23 +0800

    subcode

--- chapter01/02_basis.ts
+++ chapter01/02_basis.ts
@@ -1,18 +1,7 @@
-let a: unknown = 1; // ✓ 正确
-let b: unknown = a;
-let c: any = a;
-let e: string = a as string;
-function fn2 (): never {
-    throw new Error('报错了');
+
+enum Gender {
+    Male,
+    Female,
 }
-
-function fn3 (): never {
-    while(true){}
-}
-
-let x: () => void = function () {
-
-};
-
-// let s: string = x;
-// let s: string = c;
+let a : { name: string, gender: Gender };
+a = { name: '张三', gender: Gender.Male };
