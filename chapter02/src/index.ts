eac039b64127028da55117af3e3fb37160f33896
Woshiajuana <zhigang.chen@owulia.com>
2021-04-22 21:50:45 +0800

    subcode

---
+++ chapter02/src/index.ts
@@ -0,0 +1,11 @@
+
+const obj = { x: 1, y: 2 };
+
+const a = { ...obj, z: 3 };
+
+console.log(obj, a);
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
--- chapter02/webpack.config.js
+++ chapter02/webpack.config.js
@@ -1,9 +1,13 @@
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 
-    mode: 'development',
+    // mode: 'development',
+    mode: 'production',
 
     entry: './src/index.ts',
 
@@ -20,6 +24,35 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
+    },
+
+    // 配置 Webpack 插件
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            // 模板
+            template: path.resolve(__dirname, 'public/index.html'),
+            minify: {
+                removeAttributeQuotes: false, // 移除属性的引号
+                removeComments: false, // 移除注释
+                collapseWhitespace: false, // 折叠空白区域
+            },
+        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             // 定义要拷贝的源目录
+        //             from: path.resolve(__dirname, 'public'),
+        //             // 定义要拷贝到的目标目录
+        //             to: path.resolve(__dirname, 'dist'),
+        //         },
+        //     ],
+        // }),
+    ],
+
+    // 用来设置引用模块
+    resolve: {
+        extensions: [ '.ts', '.js' ]
     },
 
 };
