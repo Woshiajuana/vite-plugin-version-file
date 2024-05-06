bb7bdbc1c23702bd3db860208b7e69c634054b37
chenzhigang <979703986@qq.com>
2024-05-06 23:00:43 +0800

    chore: 更新页面

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import type { Plugin } from 'vite'
+import path from 'path'
 
-export default function versionFile(): Plugin {
+export interface VersionFileOptions {
+  filepath?: string
+  source?: string | (() => string | Promise<string>)
+}
+
+export default function versionFile(options: VersionFileOptions = {}): Plugin {
+  const {
+    source = 'xxx',
+    filepath = path.join(process.cwd(), 'public', 'version.json'),
+  } = options
+
   return {
     name: 'vite-plugin-version-file',
-    buildStart() {
-      console.log('buildStart => ')
+    async buildStart() {
+      console.log('11')
+      this.emitFile({
+        fileName: filepath,
+        source: typeof source === 'function' ? await source() : source,
+        type: 'asset',
+      })
+      console.log('222')
     },
   }
 }
