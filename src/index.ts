0e880822bbeb101523369865477e50ff2b8d7099
chenzhigang <979703986@qq.com>
2024-05-07 10:26:56 +0800

    chore: 更新页面

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
 import type { Plugin } from 'vite'
 import path from 'path'
+import fs from 'fs'
 
 export interface VersionFileOptions {
+  /**
+   * 生成版本文件的路径，默认为 public/version.json
+   */
   filepath?: string
+
+  /**
+   * 生成版本文件的内容，默认生成当前时间戳
+   */
   source?: string | (() => string | Promise<string>)
 }
 
 export default function versionFile(options: VersionFileOptions = {}): Plugin {
   const {
-    source = 'xxx',
+    source = () => JSON.stringify({ version: Date.now() }, null, 2),
     filepath = path.join(process.cwd(), 'public', 'version.json'),
   } = options
 
   return {
     name: 'vite-plugin-version-file',
     async buildStart() {
-      console.log('11')
-      this.emitFile({
-        fileName: filepath,
-        source: typeof source === 'function' ? await source() : source,
-        type: 'asset',
-      })
-      console.log('222')
+      mkdirp(path.dirname(filepath))
+
+      await fs.promises.writeFile(
+        filepath,
+        typeof source === 'function' ? await source() : source,
+      )
     },
+  }
+}
+
+function mkdirp(dir: string) {
+  if (!fs.existsSync(dir)) {
+    const parentDir = path.dirname(dir)
+    mkdirp(parentDir)
+    fs.mkdirSync(dir)
   }
 }
